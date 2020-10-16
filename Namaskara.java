d77282bea79c76b2846764c67d4542f8a2e0b7fc
Naseem-1998kalaburgi <72905937+Naseem-1998kalaburgi@users.noreply.github.com>
2020-10-16 10:50:39 +0530

    first commit

---
+++ Namaskara.java
@@ -0,0 +1,15 @@
+class Wish
+{
+public static void main (String[] args)
+{
+System.out.println("Namaskara");
+}
+}
+class Camera
+{
+public static void main(String[] args)
+{
+System.out.println("Camera is kodak");
+System.out.print("Camera price");
+}
+}
