4a0d8b8e461eeb41408584a3ce0dc563083314c5
Naseem-1998kalaburgi <72905937+Naseem-1998kalaburgi@users.noreply.github.com>
2020-10-17 13:52:25 +0530

    Third commit

---
+++ Task1.java
@@ -0,0 +1,18 @@
+//invoking two methods music,drinking in main method
+class Example1{
+public static void main(String[] args){
+System.out.println("Execute the follwing methods");
+music();
+drinking();
+}
+public static void music()
+{
+	System.out.println("Play some music");
+}
+public static void drinking()
+{
+	System.out.println("The amount of drinking water required to maintain good health");
+	
+}
+
+}
