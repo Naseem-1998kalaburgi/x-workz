3d001f12aeef0ec81eea771462c87cdd446b2142
Naseem-1998kalaburgi <72905937+Naseem-1998kalaburgi@users.noreply.github.com>
2020-10-20 18:44:20 +0530

    second commit

---
+++ College.java
@@ -0,0 +1,25 @@
+public class College{
+	   public static void main(String[] col)
+	   {
+		   byte noOfStaff=120;
+		   short classRooms=555;
+		   int students=12881;
+		   long addmissionFees=767544;
+		   float percentageOfStudentsclrearedExams=90.87f;
+           double percentageOfStudentsFailedInExams=8.7765979;
+		   char grade=66;
+		   boolean largeNumberOfStudetnsClearedExams=true;
+		   String str="Students cleared the exams with good marks";
+		   System.out.println(noOfStaff);
+		   System.out.println(classRooms);
+		   System.out.println(students);
+		   System.out.println(addmissionFees);
+		   System.out.println(percentageOfStudentsclrearedExams);
+		   System.out.println(percentageOfStudentsFailedInExams);
+		   System.out.println(grade);
+		   System.out.println(largeNumberOfStudetnsClearedExams);
+		   System.out.println(str);
+		   
+           		   
+	   }
+}
