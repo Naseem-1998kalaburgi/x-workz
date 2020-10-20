5739ccd5e574bc58f93eb2f8d33151ba00a3cecb
Naseem-1998kalaburgi <72905937+Naseem-1998kalaburgi@users.noreply.github.com>
2020-10-20 18:48:28 +0530

    first commit

---
+++ Plants.java
@@ -0,0 +1,24 @@
+public class Plants{
+	public static void main(String[] s)
+	{
+		byte varietyOfPlants=12;
+		short noOfPlants=1500;
+        int noOfFruits=32600;
+		long waterSupplied=75889000;
+		float fertilizerUsed=8.04f;
+        double yield=78.9876;
+        char a=65;
+        boolean plantIsGreeen=true;
+        String str="Plants can also be classified as grasses,herbaceous plants,woody shurbs,and trees";	
+        System.out.println(varietyOfPlants);
+        System.out.println(noOfPlants);
+		System.out.println(noOfFruits);
+		System.out.println(waterSupplied);
+		System.out.println(fertilizerUsed);
+		System.out.println(yield);
+		System.out.println(a);
+		System.out.println(plantIsGreeen);
+		System.out.println(str);
+	}
+	
+}
