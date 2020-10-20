c0330a2cb9c7bd631c6fd99fc4d7c20532f046d9
Naseem-1998kalaburgi <72905937+Naseem-1998kalaburgi@users.noreply.github.com>
2020-10-20 18:43:33 +0530

    first commit

---
+++ Cars.java
@@ -0,0 +1,23 @@
+public class Cars{
+	public static void main(String[] Car)
+	{
+		byte model=6;
+		short noOfCars=201;
+		int year=2017;
+		long carPrice=8865446;
+		float speedLimit=90.50f;
+		double distanceTravelled=856668.87677;
+		char c=67;
+		boolean carIsAVehicle=true;
+		String str="Car can carry small number of pepoles";
+		System.out.println(model);
+		System.out.println(noOfCars);
+		System.out.println(year);
+		System.out.println(carPrice);
+		System.out.println(speedLimit);
+		System.out.println(distanceTravelled);
+		System.out.println(c);
+		System.out.println(carIsAVehicle);
+		System.out.println(str);
+	}
+}
