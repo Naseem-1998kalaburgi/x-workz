3db8ae5d5090993f86f99207776aaa7ca9418379
Naseem-1998kalaburgi <72905937+Naseem-1998kalaburgi@users.noreply.github.com>
2020-10-20 18:45:57 +0530

    third commit

---
+++ Mobiles.java
@@ -0,0 +1,27 @@
+public class Mobiles
+{
+	public static void main(String[] mob)
+	{
+		byte mobileBrand=9;
+		short numberOfModels=257;
+		int noOfMobiles=7555;
+		long priceOfAMobile=265670;
+		float percentageOfMobilesoldOnline=78.88f;
+		double percentageOfMobilesoldInShops=18.8767879;
+		char performance=65;
+		boolean mobilePerformanceIsGood=true;
+		String str="Large number of mobile phones are sold in online"; 
+		System.out.println(mobileBrand);
+		System.out.println(numberOfModels);
+		System.out.println(noOfMobiles);
+		System.out.println(priceOfAMobile);
+		System.out.println(percentageOfMobilesoldOnline);
+		System.out.println(percentageOfMobilesoldInShops);
+		System.out.println(performance);
+		System.out.println(mobilePerformanceIsGood);
+		System.out.println(str);
+		
+		
+	}
+}
+
