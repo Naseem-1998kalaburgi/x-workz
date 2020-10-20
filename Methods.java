90cb38ceab9f800eeb6220ef2ad4e85730a2d9ac
Naseem-1998kalaburgi <72905937+Naseem-1998kalaburgi@users.noreply.github.com>
2020-10-20 18:35:47 +0530

    first commit

---
+++ Methods.java
@@ -0,0 +1,18 @@
+public class Methods{
+	public static void main(String[] m)
+	{
+		System.out.println("Execute the following methods");
+		printEmail("naseem.kalaburgi@gmail.com");
+		printEmail("xworkz@gmail.com");
+		printProgrammingLang("java");
+		printProgrammingLang("c programming language");
+	}
+	public static void printEmail(String email)
+	{
+		System.out.println("EmailId :"+email);
+	}
+	public static void printProgrammingLang(String lang)
+	{
+		System.out.println("Programming Language :"+lang);
+	}
+}
