996814eec7ecff25495cc57b2cfd1ced71fac261
Naseem-1998kalaburgi <72905937+Naseem-1998kalaburgi@users.noreply.github.com>
2020-10-21 20:43:59 +0530

    first commit

---
+++ BooleanArray.java
@@ -0,0 +1,30 @@
+public class BooleanArray{
+	public static void main(String[] b)
+	{
+		System.out.println("Invoked main method by JVM");
+		booleanArray();
+		System.out.println("Exiting main method");
+	}
+	public static void booleanArray()
+	{
+		System.out.println("Invoked booleanArray method");
+		boolean javaIsPlatformIndependent=true;
+		boolean javaIsNotSecure=false;
+		boolean javaIsObjectOriented=true;
+		boolean javaIsNotMultithreaded=false;
+		
+		boolean[] booleanValues={true,false,true,false};
+		
+		boolean boolValueAtIndex1=booleanValues[0];
+		System.out.println("Value at index one:"+boolValueAtIndex1);
+		
+		boolean boolValueAtIndex2=booleanValues[1];
+		System.out.println("Value at index two:"+boolValueAtIndex2);
+		
+		boolean boolValueAtIndex3=booleanValues[2];
+		System.out.println("Value at index three:"+boolValueAtIndex3);
+		
+		boolean boolValueAtIndex4=booleanValues[3];
+		System.out.println("Value at index four:"+boolValueAtIndex4);
+	}
+}
