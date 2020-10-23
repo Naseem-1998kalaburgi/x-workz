9b80e78622cbb9d2b34fc7ccf672f1d91f69a323
Naseem-1998kalaburgi <72905937+Naseem-1998kalaburgi@users.noreply.github.com>
2020-10-23 21:06:17 +0530

    second commit

---
+++ MyClass.java
@@ -0,0 +1,9 @@
+public class MyClass{
+	public static void trainees(String[] trainee)
+	{
+		
+		System.out.println("Trainee name is :"+trainee[0]);
+		System.out.println("Trainee name is :"+trainee[1]);
+	}
+
+}
