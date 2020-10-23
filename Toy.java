c8314c13e18733464f71e88320a365ffaacc1b38
Naseem-1998kalaburgi <72905937+Naseem-1998kalaburgi@users.noreply.github.com>
2020-10-23 21:08:46 +0530

    second commit

---
+++ Toy.java
@@ -0,0 +1,12 @@
+public class Toy{
+	  public static void main(String[] args)
+	  {
+		  System.out.println("JVM invoked main method from Toy");
+		  
+		  Pubg.supply();
+		  
+		  Pubg.enemiesAhead();
+		  
+		  System.out.println("EXIT :: main from Toy");
+	  }
+}
